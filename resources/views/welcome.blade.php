c8fb35bc63c3ed428d47d4bf422a884ba4dda0d4
cblack90 <46687746+cblack90@users.noreply.github.com>
2021-02-06 22:57:00 -0700

    added Vue and Auth

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -35,7 +35,9 @@
                     @endauth
                 </div>
             @endif
-
+            <div>
+                <button onclick="traverseBookmarks()">Prese me</button>
+            </div>
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
                     <svg viewBox="0 0 651 192" fill="none" xmlns="http://www.w3.org/2000/svg" class="h-16 w-auto text-gray-700 sm:h-20">
@@ -128,5 +130,22 @@
                 </div>
             </div>
         </div>
+        <script>
+        var bookmarkTreeNodes = chrome.bookmarks.getTree(
+            function(bookmarkTreeNodes) {
+            console.log(bookmarkTreeNodes);
+            });
+        }
+        function traverseBookmarks(bookmarkTreeNodes) {
+            for(var i=0;i<bookmarkTreeNodes.length;i++) {
+            console.log(bookmarkTreeNodes[i].title, bookmarkTreeNodes[i].url ? bookmarkTreeNodes[i].url : "[Folder]");
+
+            if(bookmarkTreeNodes[i].children) {
+                traverseBookmarks(bookmarkTreeNodes[i].children);
+            } 
+        }
+        }
+    </script>
     </body>
+    
 </html>
